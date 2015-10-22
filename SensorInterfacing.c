72ee83ef797642c86996f3697f8e06435550d4cc
rkotwani <rohankotwani@gmail.com>
2015-10-22 17:02:29 -0500

    Update SensorInterfacing.c

--- SensorInterfacing.c
+++ SensorInterfacing.c
@@ -4,17 +4,18 @@
 #define SONAR_NUM     2 // Number or sensors.
 #define MAX_DISTANCE 200 // Max distance in cm.
 #define PING_INTERVAL 200 // Milliseconds between pings.
- int incomingByte = 0; 
+byte incomingByte = 0; 
 unsigned long pingTimer[SONAR_NUM]; // When each pings.
 unsigned int cm[SONAR_NUM]; // Store ping distances.
 uint8_t currentSensor = 0; // Which sensor is active.
  
 NewPing sonar[SONAR_NUM] = { // Sensor object array.
-  NewPing(2, 2, MAX_DISTANCE),
+  NewPing(3, 3, MAX_DISTANCE),
   NewPing(4, 4, MAX_DISTANCE),
  };
 void setup() {
-  
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
   Serial.begin(9600);
   pingTimer[0] = millis() + 75;           // First ping starts at 75ms, gives time for the Arduino to chill before starting.
   for (uint8_t i = 1; i < SONAR_NUM; i++) // Set the starting time for each sensor.
@@ -33,21 +34,39 @@ void loop() {
   }
   // Other code that *DOESN'T* analyze ping results can go here.
 }
-
 void echoCheck() { // If ping received, set the sensor distance to array.
   if (sonar[currentSensor].check_timer())
     cm[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
 }
 
-void oneSensorCycle() { // Sensor ping cycle complete, do something with the results.
-  // The following code would be replaced with your code that does something with the ping results.
-//  Serial.println(cm[0]);
-if( cm[0] < 28 && cm[0] > 3){
-    Serial.println(1);
+byte x = 0x00;
+void oneSensorCycle() { // Sensor ping cycle complete,
+//do something with the ping results.
+if( cm[0] < 35 && cm[0] > 3 && cm[1] < 15 && cm[1] > 3){
+    x |= 0xFF;
+   Serial.write(0xFF);
 }
-if( cm[1] >15){
-     Serial.println(10);
+else if( cm[0] > 35 && cm[1] < 15 && cm[1] > 3){
+   x &= 0x0C;
+   x |= 0x0C;
+   Serial.write(x);
+}
+else if( cm[0] < 35 && cm[0] > 3 && cm[1] > 15){
+   x &= 0xF0;
+   x |= 0xF0;
+   Serial.write(0xF0);
+}
+else if( cm[0] > 35  && cm[1] > 15){
+  x &= 0x00;    
+  Serial.write(0x00);
+}
+
+//     Serial.println(4);
+//Serial.println(cm[0]);
+//Serial.print(cm[0]);
+//Serial.print(' ');
+//Serial.print(cm[1]);
+//Serial.println();
 }
 
 
-}
