0b6a5b2828f244b41042a3237f18085f78d1bae9
rkotwani <rohankotwani@gmail.com>
2015-10-27 18:20:51 -0500

    Update and rename ZumoControl.c to ZumoControlWithInterrupts.c

--- ZumoControlWithInterrupts.c
+++ ZumoControlWithInterrupts.c
@@ -9,18 +9,19 @@
 #include <avr/interrupt.h>
 ZumoBuzzer buzzer;
 
+#define F_CPU 16000000
+#define USART_BAUDRATE 9600
+#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16))) - 1)
+
 ZumoReflectanceSensorArray reflectanceSensors;
 ZumoMotors motors;
 Pushbutton button(ZUMO_BUTTON);
 int lastError = 0;
-const int MAX_SPEED = 200;
-byte c;
-int rxpin = 0;
+const int MAX_SPEED = 300;
+int corrector;
 
 void setup() {
-  pinMode(1, INPUT);
-  pinMode(6, INPUT);
-  Serial.begin(9600);
+  //Serial.begin(9600);
   button.waitForButton();
 
   reflectanceSensors.init();
@@ -40,52 +41,42 @@ void setup() {
   motors.setSpeeds(0,0);
   button.waitForButton();
   
-  pinMode(rxpin, INPUT);  //sets serial pin to receive data
-  
-  UCSR0B |= (1 << RXCIE0);  //sets RXCIE0 in register UCSR0B to 1, to enable USART reception interrupt.
+  cli();
 
-   sei();  //Enable global interrupts
+    UBRR0H = BAUD_PRESCALE >> 8;
+    UBRR0L = BAUD_PRESCALE ;
+    
+    UCSR0B = (1 << RXCIE0)|(1 << RXEN0);
+    UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+    sei();
    
  
 }
 
-ISR(USART_RXC_vect){  //the USART receive Interrupt Service Routine
- if (UDR0 = 255){ 
- buzzer.play(">g32>>a32");}
-else if(UDR0 = 0x00){ buzzer.play(">g32>>b32");}
-else if(UDR0 = 0x0F){ buzzer.play(">g32>>c32");}
-else if(UDR0 = 0xF0){ buzzer.play(">g32>>d32");}
+ISR(USART_RX_vect){  //the USART receive Interrupt Service Routine
+char c = UDR0;
+          //linefollow();
+     //   } 
+     if(c == 0xf0)
+     {
+     stop();
+     }
+     else if(c == 0xff)
+     {
+     stop();
+     }
+     else if(c == 0x00)
+     {
+       linefollow();
+     }
+     else if(c = 0x0C)
+     {
+       stop();
+     }
 }
 
 void loop() {
-//  int a = 0;
-//  int b = 0;
-//  a = digitalRead(6);
-//  b = digitalRead(7);
-//  
-//  if( a == LOW && b==LOW){
-//    linefollow();
-//  }
-//  else if( a == HIGH && b==LOW){
-//    changeleftlane();
-//  }
-  
-   while (Serial.available()) {
-      delay(10);  //small delay to allow input buffer to fill
-
-      c = Serial.read();  //gets one byte from serial buffer
-      Serial.print(c);
-     } //makes the string readString
-
-       if (c == 0 || c==12)   //Match ID 3
-          {   //do something with it...
-          linefollow();
-  
-           }
-        else if(c = 240){
-         stop();
-         //buzzer.play(">g32>>b32");
-        } 
+  linefollow();
 
 }
   
@@ -95,10 +86,10 @@ void loop() {
 
 
  void slowlinefollow(){
- SLOW_SPEED = 200;
+const int SLOW_SPEED = 100;
   unsigned int sensors[6];
   int position = reflectanceSensors.readLine(sensors);
-  int error = position - 2500;
+  int error = position - 1000;
  int speedDifference = error / 4 + 6 * (error - lastError);
  lastError = error;
    int m1Speed = SLOW_SPEED + speedDifference;
@@ -106,7 +97,7 @@ void loop() {
  if (m1Speed < 0)
     m1Speed = 0;
   if (m2Speed < 0)
-    m2Speed = 0
+    m2Speed = 0;
   if (m1Speed > SLOW_SPEED)
     m1Speed = SLOW_SPEED;
   if (m2Speed > SLOW_SPEED)
@@ -137,17 +128,22 @@ void linefollow()//
 void changeleftlane(){
      motors.setLeftSpeed(-100);
      motors.setRightSpeed(200);
-     delay(500);
+     delayMicroseconds(15); 
 }
 void changerightlane(){
      motors.setLeftSpeed(200);
      motors.setRightSpeed(-100);
-     delay(500);
+     delay(10);
 }
 void stop()//stop both motors
 {
      motors.setLeftSpeed(0);
      motors.setRightSpeed(0); 
 }
-  
+void slow()
+{
+   motors.setLeftSpeed(200);
+   motors.setRightSpeed(200);
+   delay(1);
+}
 
