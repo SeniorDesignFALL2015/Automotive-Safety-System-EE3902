484bc0094ece05fdff4d33178e4ab1cf2b2d7fef
rkotwani <rohankotwani@gmail.com>
2015-11-17 16:32:15 -0600

    Update OneSensorZumo.c

--- OneSensorZumo.c
+++ OneSensorZumo.c
@@ -27,7 +27,7 @@ ZumoMotors motors;
 Pushbutton button(ZUMO_BUTTON);
 int lastError = 0;
 int line_pos = 0;
-const int MAX_SPEED = 300;
+const int MAX_SPEED = 200;
 unsigned long pingTimer[SONAR_NUM]; // Holds the times when the next ping should happen for each sensor.
 unsigned int cm[SONAR_NUM];         // Where the ping distances are stored.
 uint8_t currentSensor = 0;          // Keeps track of which sensor is active.
@@ -37,8 +37,9 @@ NewPing sonar[SONAR_NUM] = {     // Sensor object array.
 };
 
 void setup() {
+/*Set baud rate */
 
-  // Wait for the user button to be pressed and released
+//  // Wait for the user button to be pressed and released
   button.waitForButton();
 
   reflectanceSensors.init();
@@ -57,15 +58,18 @@ void setup() {
     delay(20);
   }
   motors.setSpeeds(0,0);
-  button.waitForButton();
+
   
-  Serial.begin(9600);    // start serial communication at 9600 baud
+ // Serial.begin(9600);    // start serial communication at 9600 baud
   pingTimer[0] = millis() + 75;           // First ping starts at 75ms, gives time for the Arduino to chill before starting.
   for (uint8_t i = 1; i < SONAR_NUM; i++) // Set the starting time for each sensor.
     pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL; 
+
 }
 
+
 void loop() {
+ 
   for (uint8_t i = 0; i < SONAR_NUM; i++) { // Loop through all the sensors.
     if (millis() >= pingTimer[i]) {         // Is it this sensor's time to ping?
       pingTimer[i] += PING_INTERVAL * SONAR_NUM;  // Set next time this sensor will be pinged.
@@ -89,14 +93,16 @@ void loop() {
   if (m2Speed < 0)
     m2Speed = 0;
   if (m1Speed > MAX_SPEED)
-    m1Speed = MAX_SPEED;
+  m1Speed = MAX_SPEED;
   if (m2Speed > MAX_SPEED)
     m2Speed = MAX_SPEED;
      motors.setSpeeds(m1Speed, m2Speed);
     
-     if (cm[0] < 30 && cm[0] >1 ){
+     if (cm[0] < 20 && cm[0] >3 ){
         motors.setSpeeds(0, 0);
         delay(20);
+
+
      }
 
   
@@ -111,66 +117,4 @@ void echoCheck() { // If ping received, set the sensor distance to array.
 void oneSensorCycle() { // Sensor ping cycle complete, do something with the results.
   // The following code would be replaced with your code that does something with the ping results.
 
-  for (uint8_t i = 0; i < SONAR_NUM; i++) {
-    Serial.print(i);
-    Serial.print("=");
-    Serial.print(cm[i]);
-    Serial.print("cm ");
-  }
-  Serial.println();
 }
-void restlinepos(){
-  if(line_pos == 1){
-    line_pos == 0;
-  }
-  else if(line_pos == 0){
-     line_pos ==1; 
-  }
-}
- void linefollow(){// turns on drive motor in forward and leaves it on
-
-
-  
-}
-void linefollow2()//
-{// turns on drive motor in reverse and leaves it on
-    unsigned int sensors[6];
-  int position = reflectanceSensors.readLine(sensors);
-  int error = position - 1000;
- int speedDifference = error / 4 + 6 * (error - lastError);
- lastError = error;
-   int m1Speed = MAX_SPEED + speedDifference;
-  int m2Speed = MAX_SPEED - speedDifference;
- if (m1Speed < 0)
-    m1Speed = 0;
-  if (m2Speed < 0)
-    m2Speed = 0;
-  if (m1Speed > MAX_SPEED)
-    m1Speed = MAX_SPEED;
-  if (m2Speed > MAX_SPEED)
-    m2Speed = MAX_SPEED;
-      
-     motors.setSpeeds(m1Speed, m2Speed);
-}
-
-void changeleftlane(){
-     motors.setLeftSpeed(-100); //change to left lane
-     motors.setRightSpeed(200);
-     delay(500);
-  }
- 
- void changerightlane(){
-     motors.setLeftSpeed(200);
-     motors.setRightSpeed(-100);
-     delay(500);
-
-}
-
-
-void stop()//stop both motors
-{
-     motors.setLeftSpeed(0);
-     motors.setRightSpeed(0);
-     
-}
-
