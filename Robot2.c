3b24281809e6ac8fdfd30d05c5e5f58f0fb585a0
rkotwani <rohankotwani@gmail.com>
2015-11-18 16:39:26 -0600

    Create Robot2.c

---
+++ Robot2.c
@@ -0,0 +1,122 @@
+/*
+ * Demo line-following code for the Pololu Zumo Robot
+ *
+ * This code will follow a black line on a white background, using a
+ * PID-based algorithm.  It works decently on courses with smooth, 6"
+ * radius curves and has been tested with Zumos using 30:1 HP and
+ * 75:1 HP motors.  Modifications might be required for it to work
+ * well on different courses or with different motors.
+ *
+ * http://www.pololu.com/catalog/product/2506
+ * http://www.pololu.com
+ * http://forum.pololu.com
+ */
+
+#include <QTRSensors.h>
+#include <ZumoReflectanceSensorArray.h>
+#include <ZumoMotors.h>
+#include <ZumoBuzzer.h>
+#include <Pushbutton.h>
+
+/// Misc ///
+ZumoBuzzer buzzer;
+unsigned long time;
+unsigned long n[]= {1,2,3};
+/// Zumo Motor Code ///
+ZumoReflectanceSensorArray reflectanceSensors;
+ZumoMotors motors;
+Pushbutton button(ZUMO_BUTTON);
+int lastError = 0;
+const int MAX_SPEED = 100;
+
+
+void setup()
+{
+  // Play a little welcome song
+  buzzer.play(">g32>>c32");
+
+  // Initialize the reflectance sensors module
+  reflectanceSensors.init();
+
+  // Wait for the user button to be pressed and released
+  button.waitForButton();
+
+  // Turn on LED to indicate we are in calibration mode
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+
+  // Wait 1 second and then begin automatic sensor calibration
+  // by rotating in place to sweep the sensors over the line
+  delay(1000);
+  int i;
+  for(i = 0; i < 80; i++)
+  {
+    if ((i > 10 && i <= 30) || (i > 50 && i <= 70))
+      motors.setSpeeds(-200, 200);
+    else
+      motors.setSpeeds(200, -200);
+    reflectanceSensors.calibrate();
+
+    // Since our counter runs to 80, the total delay will be
+    // 80*20 = 1600 ms.
+    delay(20);
+  }
+  motors.setSpeeds(0,0);
+
+  // Turn off LED to indicate we are through with calibration
+  digitalWrite(13, LOW);
+
+
+  // Wait for the user button to be pressed and released
+  button.waitForButton();
+
+}
+
+void loop()
+{
+
+time = millis();
+
+if(time <= 30000UL){
+stopm();
+}
+
+if(time > 30000UL){
+linefollow();
+}
+
+}
+
+void linefollow()//
+{// turns on drive motor in reverse and leaves it on
+    unsigned int sensors[6];
+  int position = reflectanceSensors.readLine(sensors);
+  int error = position - 2500;
+int speedDifference = error / 4 + 6 * (error - lastError);
+lastError = error;
+   int m1Speed = MAX_SPEED + speedDifference;
+  int m2Speed = MAX_SPEED - speedDifference;
+if (m1Speed < 0)
+    m1Speed = 0;
+  if (m2Speed < 0)
+    m2Speed = 0;
+  if (m1Speed > MAX_SPEED)
+    m1Speed = MAX_SPEED;
+  if (m2Speed > MAX_SPEED)
+    m2Speed = MAX_SPEED;
+     motors.setSpeeds(m1Speed, m2Speed);
+}
+
+
+void stopm()//stop both motors
+{
+     motors.setLeftSpeed(0);
+     motors.setRightSpeed(0);
+}
+
+void arc(){
+     motors.setLeftSpeed(50);
+     motors.setRightSpeed(200);
+     delay(700);
+}
+
