38a2e8c430aadfb14f61cfcc443c5a157637bdb7
rkotwani <rohankotwani@gmail.com>
2015-11-23 09:07:59 -0600

    Update BB_RSensor.c

--- BB_RSensor.c
+++ BB_RSensor.c
@@ -1,23 +1,22 @@
 #include <NewPing.h>
 #include <Average.h>
 
-#define SONAR_NUM     3 // Number or sensors.
+#define SONAR_NUM     2 // Number or sensors.
 #define MAX_DISTANCE 100 // Max distance in cm.
 #define PING_INTERVAL 100 // Milliseconds between pings.
-bool flag = 0; 
+bool flag = 0;   
 bool flag2 = 0;
 unsigned long pingTimer[SONAR_NUM]; // When each pings.
 unsigned long avgping0;
 unsigned long avgping1;
 unsigned int cm[SONAR_NUM]; // Store ping distances.
 uint8_t currentSensor = 0; // Which sensor is active.
-Average<unsigned int> ave(5);
-
+Average<unsigned int> ave(10);
+Average<unsigned int> ave1(10);
  
 NewPing sonar[SONAR_NUM] = { // Sensor object array.
   NewPing(3, 3, MAX_DISTANCE),
   NewPing(6, 6, MAX_DISTANCE),
-  NewPing(9, 9, MAX_DISTANCE),
  };
  
  
@@ -40,58 +39,43 @@ void loop() {
       sonar[currentSensor].ping_timer(echoCheck); // Do the ping (processing continues, interrupt will call echoCheck to look for echo).
      }
   }
-ave.push(cm[0]);
-for(int i = 0; i < 5; i++) {
-ave.get(i);
-
- }
-
 }
 void echoCheck() { // If ping received, set the sensor distance to array.
   if (sonar[currentSensor].check_timer()){
     cm[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
   }else{
-    cm[currentSensor]=MAX_DISTANCE;  }
+    cm[currentSensor]= MAX_DISTANCE;  }
 }
 
 
 void oneSensorCycle() { // Sensor ping cycle complete,
 //do something with the ping results.
-avgping0= ave.mean();
+if(cm[0] > 0 ){
+avgping0= ave.rolling(cm[0]);
+}
+if(cm[0] = 0 ){
+avgping0= MAX_DISTANCE;
+}
+if(cm[1] > 0){
+avgping1= ave1.rolling(cm[1]);
+}
+if(cm[0] = 0){
+  avgping1=MAX_DISTANCE;
+}
 
 
-if( avgping0 <= 50  && avgping0 > 5 && cm[1] <= 15 && cm[1] > 2 && cm[2] > 12 ){
+if( avgping0 <= 35  && avgping0 > 5 && avgping1 <= 15 && avgping1 > 5){
    Serial.write(0xFF);
 }
-else if( avgping0 > 50 && cm[1] <= 15 && cm[1] > 2 && cm[2] > 12 ){
+else if( avgping0 > 35 && avgping1 <= 15 && avgping1 > 5){
    Serial.write(0x0C);
 }
-else if( avgping0 <= 50 && avgping0 > 5 && cm[1] > 15 && cm[2] > 12 ){
+else if( avgping0 <= 35 && avgping0 > 5 && avgping1 > 15){
    Serial.write(0xF0);
 }
-else if( avgping0 > 50  && cm[1] > 15 && cm[2] > 12 ){  
+else if( avgping0 > 35  && avgping1 > 15){  
   Serial.write(0x00);
 }
-//// SA ////
-else if( avgping0 <= 50  && avgping0 > 5 && cm[1] <= 15 && cm[1] > 2 && cm[2] <= 12 ){
-   Serial.write(0xFF);
 }
-/// NF SS ///
-else if( avgping0 > 50 && cm[1] <= 15 && cm[1] > 2 && cm[2] <= 12 ){
-   Serial.write(0x00);
-}
-/// SF NS ///
-else if( avgping0 <= 50 && avgping0 > 5 && cm[1] > 15 && cm[2] <= 12 ){
-   Serial.write(0xF0);
-}
-/// NF NS ///
-else if( avgping0 > 50  && cm[1] > 15 && cm[2] <= 12 ){  
-  Serial.write(0x00);
-}
-
-
-}
-
-
 
 
